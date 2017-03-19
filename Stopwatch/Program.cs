175e96be3231c0bd2cd411bb5565a2eb9539a65e
sahanes <sahanes_2000@yahoo.com>
2017-03-18 22:57:46 -0400

    Initial

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stopwatch
 {
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            var stopwatch = new Stopwatch();
+            string menu = "To Start Stopwatch, Press: 1\nTo Stop  Stopwatch, Press: 2\nFor Current Stopwatch Time, Press: 3\nTo Reset Stopwatch, Press: 4\nTo Exit  Stopwatch, Press: 5";
+            string inputError = "Invalid input! Please input valid number.";
+            int response = 0;
+
+
+            Console.WriteLine("Stopwatch State is: {0}, Stopwatch time is: {1}\n"+menu,stopwatch.State, stopwatch.Duration);
+            if (Int32.TryParse(Console.ReadLine(), out response))
+            {
+                Convert.ToInt32(Console.ReadLine());
+            }
+
+            while (response != 5)
+            {
+
+                if (response == 1)
+                {
+                    stopwatch.Start();
+                }
+                else if (response == 2)
+                {
+                    stopwatch.Stop();
+                }
+
+                else if (response == 3)
+                {
+                    stopwatch.TimeCheck();
+                }
+
+                else if (response == 4)
+                {
+                    stopwatch.ResetStopwatch();
+                }
+
+                else if (response == 5)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine(inputError);
+                }
+                if (response != 0)
+                {
+                    Console.WriteLine("Stopwatch State is: {0}, Stopwatch time is: {1}\n" + menu, stopwatch.State, stopwatch.Duration);
+                    response = Convert.ToInt16(Console.ReadLine());
+                }
+            }
         }
+
     }
 }
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; private set; }
+        public DateTime StopTime { get; private set; }
+        public TimeSpan Duration { get; private set; } = new TimeSpan(0, 0, 0);
+        public bool Running { get; private set; } = false;
+        public string State { get; private set; } = "Stopped";
+        private TimeSpan _zeroTime = new TimeSpan(0, 0, 0);
+        //private TimeSpan _pauseTime = new TimeSpan(0, 0, 0);
+
+
+        public Stopwatch() { }
+        
+        private void _duration()
+        {
+            /*if (Duration > _zeroTime & State == "Paused")
+            {
+                _pauseTime += StartTime - StopTime;
+            }*/
+
+            if (StartTime <= StopTime)
+            {
+                if (State == "Paused")
+                {
+                    Duration += StopTime - StartTime;// - _pauseTime;
+                }
+                else
+                {
+                    Duration = StopTime - StartTime;
+                }
+            }
+        }
+
+        private void _duration(DateTime startTime, DateTime stopTime)
+        {
+            /*if (Duration > _zeroTime & State == "Paused")
+            {
+                _pauseTime += StartTime - StopTime;
+            }*/
+
+            if (startTime <= stopTime)
+            {
+                if (State == "Paused")
+                {
+                    Duration += stopTime - startTime;// - _pauseTime;
+                }
+                else
+                {
+                    Duration = stopTime - startTime;
+                }
+            }
+        }
+
+        public void Start()
+        {
+            if (!Running)
+            {
+                Running = true;
+                State = "Running";
+                StartTime = DateTime.Now;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid input! Stopwatch is currently running.");
+            }
+            //_duration();
+        }
+
+        public void Stop()
+        {
+            if (Running)
+            {
+                Running = false;
+                State = "Paused";
+                StopTime = DateTime.Now;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid input! Stopwatch has not been started.");
+            }
+            _duration();
+        }
+
+        public void TimeCheck()
+        {
+            //StopTime = DateTime.Now;
+            if (State == "Running")
+            {
+                _duration(StartTime, DateTime.Now);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid input! Stopwatch is not currently running.");
+            }
+        }
+
+        public void ResetStopwatch()
+        {
+            Running = false;
+            State = "Stopped";
+            Duration = _zeroTime;
+            //_pauseTime = _zeroTime;
+        }
+
+    }
+}
